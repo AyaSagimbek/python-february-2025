4ce1ae41aaa5c4e27c68a32e9b26f01f3401fbf0
Ubuntu <ubuntu@ip-172-31-28-239.ec2.internal>
2025-07-02 22:48:55 +0000

    ggg

---
+++ conditionals.py
@@ -0,0 +1,17 @@
+x=5
+y=7
+
+if x == 5 and y > 6 and x < y and 10 > 2 and x != 6 and x > y:
+    print("hello")
+elif x > 3:
+    print("World")
+else:
+    print("hi")
+
+
+
+#bash:
+#if [ $x -eq 5]
+#then
+#  echo "hello"
+#fi
